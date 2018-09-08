814b89b893c9987be02f65634128080d084654a4
Valeri0n117 <Valeri0n117@gmail.com>
2018-09-08 17:47:54 +0200

    function declaration

--- kmeans_2D/openCL/kmeans.c
+++ kmeans_2D/openCL/kmeans.c
@@ -7,6 +7,46 @@ int error(const char *msg) {
 }
 
 
+size_t preferred_wg_initPoints;
+
+cl_event initPoints(cl_command_queue que, cl_kernel initPoints_k, cl_int nPoints,
+						cl_mem points){
+	//TO DO
+}
+
+
+size_t preferred_wg_initClusterID;
+
+cl_event initClusterID(cl_command_queue que, cl_kernel initClusterID_k, cl_int nPoints
+						cl_mem clusterID){
+	//TO DO
+}
+
+
+//test to do "how many iteration will avoid a min/max ceck on centroid's initialization?"
+size_t preferred_wg_initRandomCentroids;
+
+cl_event initRandomCentroids(cl_command_queue que, cl_kernel initRandomCentroids_k, cl_int nClusters,
+								cl_mem centroid, cl_int nPoints, cl_mem points ){
+	//TO DO
+}
+
+
+size_t preferred_wg_assignPoints;
+
+cl_event assignPoints(cl_command_queue que, cl_kernel assignPoints_k, cl_int nPoints, cl_mem points
+						cl_mem clusterID, cl_mem distances, cl_int nClusters, cl_mem centroids, 
+						cl_int continue){
+	//TO DO
+}
+
+
+size_t preferred_wg_adjustCentroids(cl_command_queue que, cl_kernel adjustCentroids_k, cl_int nClusters
+									cl_mem centroids, cl_int nPoints, cl_mem points, cl_mem clusterID){
+	//TO DO
+}
+
+
 int main(int argc, char *argv[]){
 
 	if(argc != 3)
@@ -30,7 +70,7 @@ int main(int argc, char *argv[]){
 	cl_mem points = clCreateBuffer(ctx, CL_MEM_READ_WRITE, sizeof(float)*nPoints*2, NULL, &err);
 	ocl_check(err, "allocation of points");
 
-	cl_mem ClusterID = clCreateBuffer(ctx, CL_MEM_READ_WRITE, sizeof(int)*nPoints, NULL, &err);
+	cl_mem clusterID = clCreateBuffer(ctx, CL_MEM_READ_WRITE, sizeof(int)*nPoints, NULL, &err);
 	ocl_check(err, "allocation of clusterID");
 
 	cl_mem distances = clCreateBuffer(ctx, CL_MEM_READ_WRITE, sizeof(float)*nPoints, NULL, &err);
@@ -44,7 +84,7 @@ int main(int argc, char *argv[]){
 	cl_kernel initPoints_k = clCreateKernel(prog, "initPoints", &err);
 	ocl_check(err, "create kernel initPoints");
 
-	cl_kernel initClusterID = clCreateKernel(prog, "initPoints", &err);
+	cl_kernel initClusterID_k = clCreateKernel(prog, "initPoints", &err);
 	ocl_check(err, "create kernel initClusterID");
 
 	cl_kernel initRandomCentroids_k = clCreateKernel(prog, "initRandomCentroids", &err);
