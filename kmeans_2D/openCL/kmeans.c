347cad060ed41ab268501d06e45ba2f9ace830a8
Valeri0n117 <Valeri0n117@gmail.com>
2018-09-09 16:03:09 +0200

    headache avoided on random points initialization

--- kmeans_2D/openCL/kmeans.c
+++ kmeans_2D/openCL/kmeans.c
@@ -6,7 +6,7 @@ int error(const char *msg) {
 	exit(1);
 }
 
-/*  for this project the initialization spped is irrilevant, is more important to have a pseudo random set generated with a seed because
+/*  for this project the initialization speed is irrilevant, is more important to have a pseudo random set generated with a seed because
 	we need to compare the C version and the OpenCL version
 size_t preferred_wg_initPoints;
 
@@ -17,11 +17,34 @@ cl_event initPoints(cl_command_queue que, cl_kernel initPoints_k, cl_int nPoints
 
 */
 
+void initPoints(int n, float *points){
+
+	for (int i=0; i<n; i++){
+		//points[i*2]=i*10;
+		//points[i*2+1]=i*10;
+
+		points[i*2]=(float)((double)rand()/(double)(RAND_MAX/999999.0f));
+		points[i*2+1]=(float)((double)rand()/(double)(RAND_MAX/999999.0f));
+	}
+}
+
 size_t preferred_wg_initClusterID;
 
 cl_event initClusterID(cl_command_queue que, cl_kernel initClusterID_k, cl_int nPoints
 						cl_mem clusterID){
-	//TO DO
+	cl_int err;
+	size_t gws[] = {round_mul_up(nPoints, preferred_wg_initClusterID)};
+	cl_event = in_evt;
+
+	err = clSetKernelArg(initClusterID_k, 0, sizeof(nPoints), &nPoints);
+	ocl_check(err, "set initClusterID arg 0");
+	err = clSetKernelArg(initClusterID_k, 1, sizeof(points), &points);
+	ocl_check(err, "set initClusterID arg 1");queueNDRangeKernel
+
+	err = clEnqueueNDRangeKernel(que, initClusterID_k,
+									1, NULL, gws, NULL, //launch grid
+									0, NULL, &initClusterID_evt); //waitlist
+	ocl_check(err, "launching initClusterID");
 }
 
 
@@ -99,6 +122,35 @@ int main(int argc, char *argv[]){
 
 	cl_kernel adjustCentroids_k = clCreateKernel(prog, "adjustCentroids", &err);
 	ocl_check(err, "create kernel adjustCentroids");
+
+
+
+	//estimating workgroup preferred size
+
+/*	see line 9
+	err = clGetKernelWorkGroupInfo(initPoints_k, d, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE,
+									sizeof(preferred_wg_initPoints), &preferred_wg_initPoints, NULL);
+	ocl_check(err, "WG info initPoints"); */
+
+	err = clGetKernelWorkGroupInfo(initClusterID, d, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE,
+									sizeof(preferred_wg_initClusterID), &preferred_wg_initClusterID, NULL);
+	ocl_check(err, "WG info initClusterID");
+
+	err = clGetKernelWorkGroupInfo(initRandomCentroids_k, d, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE,
+									sizeof(preferred_wg_initRandomCentroids), &preferred_wg_initRandomCentroids, NULL);
+	ocl_check(err, "WG info initRandomCentroids");
+
+	err = clGetKernelWorkGroupInfo(assignPoints_k, d, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE,
+									sizeof(preferred_wg_assignPoints), &preferred_wg_assignPoints, NULL);
+	ocl_check(err, "WG info assignPoints");
+
+	err = clGetKernelWorkGroupInfo(adjustCentroids_k, d, CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE,
+									sizeof(preferred_wg_adjustCentroids), &preferred_wg_adjustCentroids, NULL);
+	ocl_check(err, "WG info adjustCentroids");
+
+
+	
+	cl_event initClusterID_evt = initClusterID(que, initClusterID_k, nPoints, clusterID);
 	
 
 
