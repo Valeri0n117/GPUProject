1beb56d85a0b2c59715a5c720f508a039521b742
Valeri0n117 <Valeri0n117@gmail.com>
2018-09-23 22:16:57 +0200

    all basic function added

--- hierarchical/c/hierarchical.c
+++ hierarchical/c/hierarchical.c
@@ -17,12 +17,12 @@ void printArray(float *array, int n){
 	return;
 }
 
-void initPoints(float * points, int n){
+void initPointsAndCentroids(float * points, float *centroids, int n){
 
 	srand((unsigned int)time(NULL));
 	for(int i=0; i<n; ++i){
-		points[i*2]=(float)((double)rand()/(double)(RAND_MAX/999999.0f));;
-		points[i*2+1]=(float)((double)rand()/(double)(RAND_MAX/999999.0f));;
+		centroids[i*2]=points[i*2]=(float)((double)rand()/(double)(RAND_MAX/999999.0f));;
+		centroids[i*2+1]=points[i*2+1]=(float)((double)rand()/(double)(RAND_MAX/999999.0f));;
 	}
 	
 }
@@ -44,11 +44,57 @@ void findMinDistances(float *distances, int nPoints, int *toMergeA, int *toMerge
 		for(int j=0; j<i; ++j){
 			if(distances[((i*(i-1))/2)+j] < tmp){
 				tmp = distances[((i*(i-1))/2)+j];
-				*toMergeA = i;
-				*toMergeB = j;
+				*toMergeA = j;
+				*toMergeB = i;
 			}
 		}
 
+}
+
+int mergeClusters(float *points, int toMergeA, int toMergeB, int *clusterID, 
+					int nPoints, float *centroids, int nCluster, float *distances){
+	//adjusting clusterID
+	for(int i=0; i < nPoints; ++i){
+		if(clusterID[i] == toMergeB)
+			clusterID[i] == toMergeA;
+	}
+
+	//adjusting centroids of toMergeA;
+	float sumX=0;
+	float sumY=0;
+	int n=0;
+
+	for(int i=0; i < nPoints; ++i){
+		if(clusterID[i] == toMergeA){
+			sumX += points[i*2];
+			sumY += points[i*2+1];
+			++n;
+		}
+	}
+
+	centroids[toMergeA*2]= sumX / n;
+	centroids[toMergeA*2+1]= sumY / n;
+
+	// ADJUST DISTANCES
+		//toMergeA's distances
+	for(int i=0; i < nCluster; ++i){
+		if(i < toMergeA)
+			distances[((toMergeA*(toMergeA-1))/2)+i] = sqrt( pow(centroids[i*2]-centroids[toMergeA*2], 2.0f) + pow(centroids[i*2+1]-centroids[toMergeA*2+1], 2.0f) );
+		else
+			distances[((i*(i-1))/2)+toMergeA] = sqrt( pow(centroids[i*2]-centroids[toMergeA*2], 2.0f) + pow(centroids[i*2+1]-centroids[toMergeA*2+1], 2.0f) );
+	}
+
+		//"removing" toMergeB distances
+	if(toMergeB != (nCluster-1))
+		for(int i=0; i<nCluster; ++i)
+			if(i < toMergeB)
+				distances[((toMergeB*(toMergeB-1))/2)+i] = distances[(((nCluster-1)*((nCluster-1)-1))/2)+i];
+			else
+				distances[((i*(i-1))/2)+toMergeB] = distances[(((nCluster-1)*((nCluster-1)-1))/2)+i];
+	
+
+
+	return --nCluster;
 }
 
 
@@ -57,18 +103,20 @@ int main(int argc, char *argv[]){
 		error("usage: hierarchical nPoints nCluster ");
 
 	int nPoints = atoi(argv[1]);
-	int nCluster = atoi(argv[2]);
+	int nCluster = nPoints;
+	int clusterObjective = atoi(argv[2]);
 
 	float *points = (float *)malloc(sizeof(float)*2*nPoints);
 	int *clusterID = (int *)malloc(sizeof(int)*nPoints);
-	//float *clusterID = (float *)malloc(sizeof(float)*2*nPoints);
+	float *centroids = (float *)malloc(sizeof(float)*2*nPoints);
 	float *distances = (float *)malloc(sizeof(float)*(nPoints*(nPoints-1))/2);
 	int toMergeA, toMergeB;
 
-	initPoints(points, nPoints);
+	initPointsAndCentroids(points, centroids, nPoints);
 	initDistances(distances, points, nPoints);
 	findMinDistances(distances, nPoints, &toMergeA, &toMergeB);
+	mergeClusters(points, toMergeA, toMergeA, clusterID, nPoints, centroids, nCluster, distances);
 
-	printf("toMergeA: %i\n toMergeB: %i\n", toMergeA, toMergeB);
+	printf("toMergeA: %i\ntoMergeB: %i\n", toMergeA, toMergeB);
 	
 }
